382c8c274cef3525223050be6ca40631c067fc97
Bibounet <bibou.ytb33@gmail.com>
2026-02-11 10:04:29 +0100

    feature:delete wallpaper from wp list

--- handlers/DeletewpHandler.go
+++ handlers/DeletewpHandler.go
@@ -7,5 +7,26 @@ import (
 
 func DeletewpHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/wallpapers", http.StatusSeeOther)
-	log.Println("you better finish this feature bib.... (delete)")
+
+	userID, err := getUserIDFromSession(r)
+	if err != nil {
+		log.Println("Session error:", err)
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	wallpaperID := r.FormValue("wallpaper_id")
+	if wallpaperID == "" {
+		http.Error(w, "Wallpaper ID missing", http.StatusBadRequest)
+		return
+	}
+
+	//delete seleccted wp
+	_, err = db.Exec("DELETE FROM wallpapers WHERE id = ?", wallpaperID)
+	if err != nil {
+		log.Println("failed to delete..", err)
+	}
+
+	log.Println("user", userID, "deleted a wp!")
+
 }
